380c1cc146bb7a5ba4b26a66b92e1c8241ae8f52
Sovitsaini143 <sovitsaini37@gmail.com>
2025-01-02 16:29:19 +0530

    savess

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import colors from "colors";
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      `Conneted To successfully `
     );
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Errro in Mongodb ${error}`);
   }
 };
 
